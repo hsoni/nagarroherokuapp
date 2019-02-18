e2f3a75738567f870440b75c7ac7cc2cac38493d
unknown <Nagarro@himan3148561.Nagarro.local>
2019-02-18 16:04:33 +0530

    test

--- server/knexfile.js
+++ server/knexfile.js
@@ -5,12 +5,22 @@ module.exports = {
 
   development: {
     client: 'pg',
-    connection: config.db_url
+    connection: {
+      host : 'ec2-35-170-4-198.compute-1.amazonaws.com',
+      user : 'u5al67l5mqiap3',
+      password : 'pebcfe53d7281c1d9f0d1639fdb1231494e04aa511744fbf281c1f438b30c43b7',
+      database : 'dbr8dvina7u4mh'
+    }
   },
 
   production: {
     client: 'pg',
-    connection: config.db_url
+    connection: {
+      host : 'ec2-35-170-4-198.compute-1.amazonaws.com',
+      user : 'u5al67l5mqiap3',
+      password : 'pebcfe53d7281c1d9f0d1639fdb1231494e04aa511744fbf281c1f438b30c43b7',
+      database : 'dbr8dvina7u4mh'
+    }
   }
 
 };
