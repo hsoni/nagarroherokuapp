9ced8071a537bd8e56cd8c891b21f9e7c025f545
unknown <Nagarro@himan3148561.Nagarro.local>
2019-02-18 15:48:48 +0530

    test

--- server/knexfile.js
+++ server/knexfile.js
@@ -4,12 +4,26 @@ var config = require('./config');
 module.exports = {
 
   development: {
-    client: 'pg',
+    client: {
+      user: "u5al67l5mqiap3",
+      password: "pebcfe53d7281c1d9f0d1639fdb1231494e04aa511744fbf281c1f438b30c43b7",
+      database: "dbr8dvina7u4mh",
+      port: 5432,
+      host: "ec2-35-170-4-198.compute-1.amazonaws.com",
+      ssl: true
+  },
     connection: config.db_url
   },
 
   production: {
-    client: 'pg',
+    client: {
+      user: "u5al67l5mqiap3",
+      password: "pebcfe53d7281c1d9f0d1639fdb1231494e04aa511744fbf281c1f438b30c43b7",
+      database: "dbr8dvina7u4mh",
+      port: 5432,
+      host: "ec2-35-170-4-198.compute-1.amazonaws.com",
+      ssl: true
+  },
     connection: config.db_url
   }
 
