bd36243c380a3087b1126096e42cf2238ec62f9c
unknown <Nagarro@himan3148561.Nagarro.local>
2019-02-18 16:06:34 +0530

    test

--- server/knexfile.js
+++ server/knexfile.js
@@ -9,7 +9,8 @@ module.exports = {
       host : 'ec2-35-170-4-198.compute-1.amazonaws.com',
       user : 'u5al67l5mqiap3',
       password : 'pebcfe53d7281c1d9f0d1639fdb1231494e04aa511744fbf281c1f438b30c43b7',
-      database : 'dbr8dvina7u4mh'
+      database : 'dbr8dvina7u4mh',
+      ssl:true
     }
   },
 
@@ -19,7 +20,8 @@ module.exports = {
       host : 'ec2-35-170-4-198.compute-1.amazonaws.com',
       user : 'u5al67l5mqiap3',
       password : 'pebcfe53d7281c1d9f0d1639fdb1231494e04aa511744fbf281c1f438b30c43b7',
-      database : 'dbr8dvina7u4mh'
+      database : 'dbr8dvina7u4mh',
+      ssl:true
     }
   }
 
